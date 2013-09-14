cc09225750a43fa491dd9b539641167975f76566
nick <macinnis.nick@gmail.com>
2013-09-13 18:47:28 -0700

    wip

---
+++ erlang/euler033.erl
@@ -0,0 +1,63 @@
+-module(euler033).
+
+-export([go/0]).
+
+go() ->
+    Fractions = find_canceling_fractions(10, 99, 10, 99),
+    io:format("~p~n", [Fractions]),
+    lists:foldl(fun({Num, Den}, Prod) -> (Den/Num) * Prod end, 1, Fractions).
+
+find_canceling_fractions(MinNum, MaxNum, MinDen, MaxDen) ->
+    Numerators = lists:seq(MinNum, MaxNum),
+    Denominators = lists:seq(MinDen, MaxDen),
+    DeepMatches = lists:map(
+            fun(Num) -> lists:filter(
+                        fun({Numer, Den}) -> is_canceling(Numer, Den) end,
+                        lists:map(
+                            fun(Den) -> {Num, Den} end,
+                            Denominators
+                            )
+                        ) end,
+            Numerators
+            ),
+    lists:flatten(DeepMatches).
+
+is_canceling(Num, Den) when (Num == Den) or (Num/Den > 1) ->
+    false;
+is_canceling(Num, Den) ->
+    % identify common digits
+    NumStr = erlang:integer_to_list(Num),
+    DenStr = erlang:integer_to_list(Den),
+    Int = sets:to_list(
+            sets:intersection(
+                sets:from_list(NumStr),
+                sets:from_list(DenStr)
+                )
+            ),
+    Matches = lists:any(
+            fun(Elem) -> Elem == (Num/Den) end,
+            lists:map(
+                fun(Elem) -> quotient_after_removing_char(NumStr, DenStr, Elem) end,
+                Int
+                )
+            ),
+    if
+        Matches ->
+            io:format("~p/~p cancel ~p matches: ~p~n", [NumStr, DenStr, Int, Matches]);
+        true ->
+            whatever
+    end,
+    Matches.
+
+quotient_after_removing_char(_NumStr, _DenStr, C) when C == $0 ->
+    -1;
+quotient_after_removing_char(NumStr, DenStr, C) ->
+    {NewNum, []} = string:to_integer(lists:delete(C, NumStr)),
+    {NewDen, []} = string:to_integer(lists:delete(C, DenStr)),
+    case NewDen of
+        0 ->
+            -1;
+        _ ->
+            NewNum/NewDen
+    end.
+
